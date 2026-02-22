0f3dea847c9b991c67aa750eb1c26c09a1935357
takumi nishimura <clp13218@nitech.jp>
2026-02-22 17:52:33 +0900

    fix(cooking-gym): include package_data for level JSONs and graphics PNGs

--- coop_marl/envs/overcooked/setup.py
+++ coop_marl/envs/overcooked/setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.0.1",
     description='Cooking gym with graphics and ideas based on: "Too Many Cooks: Overcooked environment"',
     packages=find_packages() + [""],
+    package_data={
+        "gym_cooking": [
+            "utils/new_style_level/*.json",
+            "misc/game/graphics/*.png",
+        ],
+    },
     install_requires=[],
 )
